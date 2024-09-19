b439125d1a7ca23627eae615b7db2c7b24820045
Madina <m.a.baizhanova@gmail.com>
2024-09-19 14:32:06 +0500

    Added some logging and exception handling in DAO layer based on the code reviewer's comments.

--- src/main/java/com/epam/wca/gym/dao/impl/AbstractDAO.java
+++ src/main/java/com/epam/wca/gym/dao/impl/AbstractDAO.java
@@ -1,9 +1,12 @@
 package com.epam.wca.gym.dao.impl;
 
 import com.epam.wca.gym.dao.BaseDAO;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+@Slf4j
 public abstract class AbstractDAO<T> implements BaseDAO<T> {
 
     protected SessionFactory sessionFactory;
@@ -14,21 +17,37 @@ public abstract class AbstractDAO<T> implements BaseDAO<T> {
 
     @Override
     public T save(T entity) {
-        Session session = sessionFactory.getCurrentSession();
-        session.persist(entity);
-        return entity;
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.persist(entity);
+            return entity;
+        } catch (HibernateException exception) {
+            log.error("Error occurred while saving entity.", exception);
+            throw exception;
+        }
     }
 
     @Override
     public T update(T entity) {
-        Session session = sessionFactory.getCurrentSession();
-        session.merge(entity);
-        return entity;
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.merge(entity);
+            return entity;
+        } catch (HibernateException exception) {
+            log.error("Error occurred while updating entity.", exception);
+            throw exception;
+        }
+
     }
 
     @Override
     public void delete(T entity) {
-        Session session = sessionFactory.getCurrentSession();
-        session.remove(entity);
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.remove(entity);
+        } catch (HibernateException exception) {
+            log.error("Error occurred while deleting entity.", exception);
+            throw exception;
+        }
     }
 }
--- src/main/java/com/epam/wca/gym/dao/impl/TrainingDAOImpl.java
+++ src/main/java/com/epam/wca/gym/dao/impl/TrainingDAOImpl.java
@@ -2,10 +2,13 @@ package com.epam.wca.gym.dao.impl;
 
 import com.epam.wca.gym.dao.TrainingDAO;
 import com.epam.wca.gym.entity.Training;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
+@Slf4j
 @Repository
 public class TrainingDAOImpl extends AbstractDAO<Training> implements TrainingDAO {
 
@@ -24,9 +27,14 @@ public class TrainingDAOImpl extends AbstractDAO<Training> implements TrainingDA
     }
 
     private void deleteByRole(String role, String username) {
-        Session session = sessionFactory.getCurrentSession();
-        session.createMutationQuery("DELETE FROM Training t WHERE t." + role + " = :username")
-                .setParameter("username", username)
-                .executeUpdate();
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.createMutationQuery("DELETE FROM Training t WHERE t." + role + " = :username")
+                    .setParameter("username", username)
+                    .executeUpdate();
+        } catch (HibernateException exception) {
+            log.error("Error occurred while deleting Training entities.", exception);
+            throw exception;
+        }
     }
 }
