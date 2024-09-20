472926deb5507f454cbf1245f075c294aba40a16
Madina <m.a.baizhanova@gmail.com>
2024-09-20 13:53:38 +0500

    Replaced the query concatenation in the TrainingDAOImpl by the use of Criteria API based on the comments omy code reviewer.

--- src/main/java/com/epam/wca/gym/dao/impl/TrainingDAOImpl.java
+++ src/main/java/com/epam/wca/gym/dao/impl/TrainingDAOImpl.java
@@ -1,7 +1,11 @@
 package com.epam.wca.gym.dao.impl;
 
 import com.epam.wca.gym.dao.TrainingDAO;
+import com.epam.wca.gym.entity.Role;
 import com.epam.wca.gym.entity.Training;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaDelete;
+import jakarta.persistence.criteria.Root;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -18,20 +22,28 @@ public class TrainingDAOImpl extends AbstractDAO<Training> implements TrainingDA
 
     @Override
     public void deleteByTrainee(String traineeUsername) {
-        deleteByRole("trainee.user.username", traineeUsername);
+        deleteByRole(Role.TRAINEE, traineeUsername);
     }
 
     @Override
     public void deleteByTrainer(String trainerUsername) {
-        deleteByRole("trainer.user.username", trainerUsername);
+        deleteByRole(Role.TRAINER, trainerUsername);
     }
 
-    private void deleteByRole(String role, String username) {
+    private void deleteByRole(Role role, String username) {
         try {
             Session session = sessionFactory.getCurrentSession();
-            session.createMutationQuery("DELETE FROM Training t WHERE t." + role + " = :username")
-                    .setParameter("username", username)
-                    .executeUpdate();
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaDelete<Training> delete = cb.createCriteriaDelete(Training.class);
+            Root<Training> root = delete.from(Training.class);
+
+            if (role == Role.TRAINEE) {
+                delete.where(cb.equal(root.get("trainee").get("user").get("username"), username));
+            } else if (role == Role.TRAINER) {
+                delete.where(cb.equal(root.get("trainer").get("user").get("username"), username));
+            }
+
+            session.createMutationQuery(delete).executeUpdate();
         } catch (HibernateException exception) {
             log.error("Error occurred while deleting Training entities.", exception);
             throw exception;
